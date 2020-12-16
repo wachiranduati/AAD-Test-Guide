b24081714fec18b309bd4682c958f20f239c4ba8
Wachira Nduati <wachiranduati@gmail.com>
2020-12-16 21:14:56 +0300

    add categories recyclerview and adapter

---
+++ app/src/main/java/com/nduati/aadtestguide/adapter/TestGuidelineAdapter.kt
@@ -0,0 +1,11 @@
+package com.nduati.aadtestguide.adapter
+
+import android.view.View
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.RecyclerView
+
+class TestGuidelineAdapter {
+    class GuidelineViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
+}
