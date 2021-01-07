97deb27dc8991d702dc4d900999d82f2e545b58e
Wachira Nduati <wachiranduati@gmail.com>
2021-01-07 15:04:50 +0300

    implement navigation to services fragment from android core

--- app/src/main/java/com/nduati/aadtestguide/AndroidCoreFragment.kt
+++ app/src/main/java/com/nduati/aadtestguide/AndroidCoreFragment.kt
@@ -5,13 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
 
 class AndroidCoreFragment : Fragment() {
-
+    lateinit var NavToServices : Button
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_android_core, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        NavToServices = view.findViewById(R.id.ServicesButton)
+        NavToServices.setOnClickListener {
+//            Toast.makeText(view.context, "I've been clicked", Toast.LENGTH_SHORT).show()
+            view.findNavController().navigate(R.id.action_androidCoreFragment_to_servicesFragment)
+        }
     }
 
 }
