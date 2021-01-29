c4194e7073e7141790232783ee6cdab8ebfc2b0c
Wachira Nduati <wachiranduati@gmail.com>
2021-01-29 10:04:18 +0300

    implement foreground service

--- app/src/main/java/com/nduati/aadtestguide/services/ServicesFragment.kt
+++ app/src/main/java/com/nduati/aadtestguide/services/ServicesFragment.kt
@@ -5,15 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.nduati.aadtestguide.R
+import kotlinx.android.synthetic.main.fragment_services.view.*
 
-class ServicesFragment : Fragment() {
-
+class ServicesFragment : Fragment(), View.OnClickListener {
+    lateinit var root : View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false)
+        root = inflater.inflate(R.layout.fragment_services, container, false)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        root.StartServiceButton.setOnClickListener(this)
+        root.StopServiceButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.StartServiceButton -> {
+                Toast.makeText(activity, "start service", Toast.LENGTH_SHORT).show()
+                ForegroundService.startService(v.context, "foreground service running")
+            }
+            R.id.StopServiceButton -> {
+                Toast.makeText(activity, "stop service", Toast.LENGTH_SHORT).show()
+                ForegroundService.stopService(v.context)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/nduati/aadtestguide/util/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.nduati.aadtestguide.util
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nduati.aadtestguide.R
+
+class SampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
