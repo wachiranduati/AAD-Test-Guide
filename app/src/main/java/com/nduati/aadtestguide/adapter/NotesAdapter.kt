90e7467ad5cd2a6757b3a08f88a9b100a45cba7d
Wachira Nduati <wachiranduati@gmail.com>
2021-02-05 01:50:20 +0300

    implement notes recycler adapter

--- app/src/main/java/com/nduati/aadtestguide/adapter/NotesAdapter.kt
+++ app/src/main/java/com/nduati/aadtestguide/adapter/NotesAdapter.kt
@@ -1,4 +1,28 @@
 package com.nduati.aadtestguide.adapter
 
-class NotesAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.notes_list_item.view.*
+
+class NotesAdapter() : RecyclerView.Adapter<NotesAdapter.notesHolder>() {
+
+    class notesHolder(view : View): RecyclerView.ViewHolder(view){
+        val notedID : TextView = view.notedId
+        val noteHeader : TextView = view.noteHeader
+        val noteDescription : TextView = view.noteDescription
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): notesHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: notesHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
