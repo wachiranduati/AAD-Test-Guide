29a386b012a7a3385997ad6273d9efc424488034
Wachira Nduati <wachiranduati@gmail.com>
2021-02-17 22:30:48 +0300

    add debug fragment

--- app/src/main/java/com/nduati/aadtestguide/TestCategories.kt
+++ app/src/main/java/com/nduati/aadtestguide/TestCategories.kt
@@ -5,7 +5,7 @@ internal fun TestCategories() : List<TestGuidlineCategories> {
             TestGuidlineCategories("Android Core", R.id.action_homeFragment_to_androidCoreFragment),
             TestGuidlineCategories("User Interface", R.id.action_homeFragment_to_androidCoreFragment),
             TestGuidlineCategories("Data Management", R.id.action_homeFragment_to_notesFragment),
-            TestGuidlineCategories("Debugging", R.id.action_homeFragment_to_androidCoreFragment),
+            TestGuidlineCategories("Debugging", R.id.action_homeFragment_to_debugFragment),
             TestGuidlineCategories("Testing", R.id.action_homeFragment_to_androidCoreFragment)
     )
 }
