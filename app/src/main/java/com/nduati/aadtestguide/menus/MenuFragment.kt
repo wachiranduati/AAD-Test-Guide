e040d826cf4b46471cdfe7ae015f183e1e3b0710
Wachira Nduati <wachiranduati@gmail.com>
2021-02-22 14:39:57 +0300

    implement context menu

--- app/src/main/java/com/nduati/aadtestguide/menus/MenuFragment.kt
+++ app/src/main/java/com/nduati/aadtestguide/menus/MenuFragment.kt
@@ -3,16 +3,22 @@ package com.nduati.aadtestguide.menus
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.nduati.aadtestguide.R
+import kotlinx.android.synthetic.main.fragment_menu.view.*
 
 class MenuFragment : Fragment() {
-
+    lateinit var crdVw : CardView
+    lateinit var root : View
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        root = inflater.inflate(R.layout.fragment_menu, container, false)
+        crdVw = root.findViewById(R.id.clickOnMeCardView)
+        registerForContextMenu(crdVw)
+        return root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -35,4 +41,32 @@ class MenuFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        activity?.menuInflater?.inflate(R.menu.main_options_menu, menu)
+
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.option1 -> {
+                Toast.makeText(view?.context, "option 1", Toast.LENGTH_SHORT).show()
+            }
+            R.id.option2 -> {
+                Toast.makeText(view?.context, "option 2", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        view.clickOnMeCardView.setOnClickListener {
+//            Toast.makeText(view.context, "this is it", Toast.LENGTH_SHORT).show()
+//        }
+    }
 }
