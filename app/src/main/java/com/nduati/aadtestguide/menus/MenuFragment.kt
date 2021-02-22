93f29e058b87c61e0f5fbf48eeb72e697868adc5
Wachira Nduati <wachiranduati@gmail.com>
2021-02-22 15:14:35 +0300

    implement popup menu

--- app/src/main/java/com/nduati/aadtestguide/menus/MenuFragment.kt
+++ app/src/main/java/com/nduati/aadtestguide/menus/MenuFragment.kt
@@ -2,13 +2,15 @@ package com.nduati.aadtestguide.menus
 
 import android.os.Bundle
 import android.view.*
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.nduati.aadtestguide.R
+import kotlinx.android.synthetic.main.fragment_menu.*
 import kotlinx.android.synthetic.main.fragment_menu.view.*
 
-class MenuFragment : Fragment() {
+class MenuFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
     lateinit var crdVw : CardView
     lateinit var root : View
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -63,10 +65,33 @@ class MenuFragment : Fragment() {
         return super.onContextItemSelected(item)
     }
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        view.clickOnMeCardView.setOnClickListener {
-//            Toast.makeText(view.context, "this is it", Toast.LENGTH_SHORT).show()
-//        }
+        view.popUpCardView.setOnClickListener {
+            val popup = PopupMenu(view.context, popUpCardView)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.main_options_menu, popup.menu)
+            popup.setOnMenuItemClickListener(this)
+            popup.show()
+        }
     }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.option1 -> {
+                Toast.makeText(view?.context, "option 1 popup", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.option2 -> {
+                Toast.makeText(view?.context, "option 2 popup", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else ->{
+                return false
+            }
+        }
+    }
+
 }
